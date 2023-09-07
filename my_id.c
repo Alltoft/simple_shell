e16669f6131de7abefba6085928df55b0f4d6f40
hafsachenni <hafsachenni77@gmail.com>
2023-09-07 06:39:56 -0700

    betty is not that proud hy

--- my_id.c
+++ my_id.c
@@ -1,9 +1,14 @@
 #include "main.h"
 int main (void)
 {
-	int a;
+	int a, u;
 
 	a = my_ppid();
 	prinft("my ppid is : %d", a);
 	return(0);
+
+	u = my_pid();
+
+	printf("my process id is %d\n", u);
+	return (0);
 }
---
+++ my_pid.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(void)
+{
+	a = my_pid
+
+    pid_t pid = getpid();
+    printf("my process id is %d\n", a);
+    return (0);
+}
