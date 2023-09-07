718e8412998177c3dd020a08f68e11cee09448ca
Emran <ouakouriimran@gmail.com>
2023-09-07 05:30:18 -0700

    branches

--- main.h
+++
@@ -1,8 +0,0 @@
-#ifndef MAIN
-#define MAIN
-#include <stdio.h>
-#include <stdio.h>
-#include <unistd.h>
-int my_ppid(void);
-int my_pid(void);
-#endif
--- my_ppid.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <unistd.h>
-
-int main(void)
-{
-    pid_t ppid = getppid();
-    printf("my parent process id is %d\n", ppid);
-    return (ppid);
-}
