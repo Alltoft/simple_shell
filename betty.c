4f00e4d7a95dbe53acd2287293ce1c70b4730cd5
root <root@DESKTOP-JSKJK5P.localdomain>
2023-09-17 15:06:12 +0200

    me

--- betty.c
+++ betty.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 /**
  * main - entry point
@@ -9,7 +10,7 @@
 
 int main(void)
 {
-	char *c = "hello wolrd";
+	char *c = "hello world";
 	int length = strlen(c);
 
 	write(1, c, length);
